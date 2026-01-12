58212d6da215aa3784329e9503dfa3c52a03b685
kaka <11076632@qq.com>
2026-01-12 23:26:33 +0800

    fix: 修改eslint规则

--- eslint.config.js
+++ eslint.config.js
@@ -4,13 +4,22 @@ import prettierPlugin from 'eslint-plugin-prettier';
 import tseslint from 'typescript-eslint';
 
 export const defineConfig = [
-  { include: ['**/package/**'] },
+  { ignores: ['dist', 'node_modules', 'apps'] },
   js.configs.recommended,
   ...tseslint.configs.recommended,
   {
     files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
     plugins: { js, prettier: prettierPlugin },
     languageOptions: { globals: globals.browser },
+  },
+  {
+    files: ['scripts/**/*.{js,ts}'],
+    languageOptions: {
+      globals: {
+        __dirname: 'readonly',
+        __filename: 'readonly',
+      },
+    },
   },
 ];
 
